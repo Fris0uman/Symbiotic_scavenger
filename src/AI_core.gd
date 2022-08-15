6b4d96334281aa595b27774fb2c17695f7379bf6
Fris0uman <sthapplz@gmail.com>
2022-08-15 14:54:11 +0200

    AI core can spawn more robots

--- src/AI_core.gd
+++ src/AI_core.gd
@@ -1,12 +1,21 @@
-extends RigidBody2D
+extends Body
 
 var _ressources: Array
 
+var robot_template:= preload("res://assets/Scenes/Robot.tscn")
+
 onready var depot:= $Ressource_depot
+onready var spawner:= $Spawner
+
+onready var world:= get_parent()
 onready var map:= get_node("../Navigation2D/TileMap")
 
 func _ready() -> void:
 	place_depot()
+
+func _process(_delta: float) -> void:
+	if _ressources.size() >=2:
+		make_new_bot()
 
 func place_depot()->void:
 	# Clear all cells under the depot
@@ -33,4 +42,16 @@ func _on_Ressource_depot_body_entered(body: RigidBody2D) -> void:
 		#Disable collision with Actors
 		body.set_collision_layer_bit(0,false)
 		body.set_collision_mask_bit(0,false)
+		
 		body.set_modulate(Color.dimgray)
+		
+		_ressources.append(body)
+
+func make_new_bot()->void:
+	var new_robot = robot_template.instance()
+	new_robot.position = spawner.global_position
+	print(new_robot.position)
+	world.add_child(new_robot)
+	
+	_ressources.front().queue_free()
+	_ressources.pop_front()
